5b6c986c63358ec5040f027361664341915e4ee2
davisl24 <davislocs135@gmail.com>
2026-09-05 19:56:53 +0300

    Add PrintStich portfolio data

---
+++ js/portfolio-data.js
@@ -0,0 +1,94 @@
+const PORTFOLIO_ITEMS = [
+  {
+    fails: 'krekli-basketbols.jpg',
+    alt: 'Melni basketbola krekli ar Limbaži Basketball apdruku',
+    kategorija: 'Apģērbs'
+  },
+  {
+    fails: 'jakas-coach-manager.jpg',
+    alt: 'Melnas jakas ar Coach un Manager uzrakstu apdruku',
+    kategorija: 'Apģērbs'
+  },
+  {
+    fails: 'cepures-basketbols.jpg',
+    alt: 'Cepures ar basketbola komandas personalizētu apdruku',
+    kategorija: 'Apģērbs'
+  },
+  {
+    fails: 'cepure-boxing-tuvplans.jpg',
+    alt: 'Tuvplānā cepure ar Boxing tematikas apdruku',
+    kategorija: 'Apģērbs'
+  },
+  {
+    fails: 'auto-scania-prieksa.jpg',
+    alt: 'Scania kravas auto priekšpuse ar personalizētu aplikāciju',
+    kategorija: 'Auto aplikācija'
+  },
+  {
+    fails: 'auto-scania-sans.jpg',
+    alt: 'Scania kravas auto sānskats ar uzņēmuma aplikāciju',
+    kategorija: 'Auto aplikācija'
+  },
+  {
+    fails: 'auto-aplikacija-darbs.jpg',
+    alt: 'Auto aplikācijas uzklāšanas process darbnīcā',
+    kategorija: 'Auto aplikācija'
+  },
+  {
+    fails: 'auto-aplikacija-komanda.jpg',
+    alt: 'PrintStich komanda pie auto aplikācijas darba',
+    kategorija: 'Auto aplikācija'
+  },
+  {
+    fails: 'auto-logo-bub.jpg',
+    alt: 'Auto virsbūve ar uzklātu BUB logo aplikāciju',
+    kategorija: 'Auto aplikācija'
+  },
+  {
+    fails: 'kruzes-45gadi.jpg',
+    alt: 'Personalizētas krūzes 45 gadu jubilejai',
+    kategorija: 'Suvenīri'
+  },
+  {
+    fails: 'nozimes-45gadi.jpg',
+    alt: 'Personalizētas nozīmītes 45 gadu jubilejai',
+    kategorija: 'Suvenīri'
+  },
+  {
+    fails: 'akrils-vardu-topperi.jpg',
+    alt: 'Akrila vārdu topperi personalizētām dāvanām un dekorācijām',
+    kategorija: 'Akrils'
+  },
+  {
+    fails: 'akrils-davana-skolotajai.jpg',
+    alt: 'Personalizēta akrila dāvana skolotājai',
+    kategorija: 'Akrils'
+  },
+  {
+    fails: 'maisini-printstich.jpg',
+    alt: 'PrintStich zīmola maisiņi ar drukātu logotipu',
+    kategorija: 'Iepakojums'
+  },
+  {
+    fails: 'uzlimes-motokross.jpg',
+    alt: 'Motokrosa tematikas personalizētu uzlīmju komplekts',
+    kategorija: 'Druka'
+  },
+  {
+    fails: 'foto-druka.jpg',
+    alt: 'Izdrukāta fotogrāfija kvalitatīvā krāsu drukā',
+    kategorija: 'Druka'
+  },
+  {
+    fails: 'termoprese-darba.jpg',
+    alt: 'Termoprese darbībā apdrukas sagatavošanas procesā',
+    kategorija: 'Darbnīca'
+  },
+  {
+    fails: 'izkartne-printstich.jpg',
+    alt: 'PrintStich izkārtne pie uzņēmuma darbnīcas',
+    kategorija: 'Darbnīca'
+  }
+];
+
+window.PORTFOLIO_ITEMS = PORTFOLIO_ITEMS;
